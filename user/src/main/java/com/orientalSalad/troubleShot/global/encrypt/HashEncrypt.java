d942ae19123273557e16b3a78fc9da7a8b155116
SuHyun Kim <khnemu@naver.com>
2023-10-20 17:56:31 +0900

    feat : 비밀번호를 sha-256으로 암호화
    1. 입력받은 비밀번호를 sha-256으로 암호화하여 저장하는 기능 추가
    
    231020 suhyun

---
+++ user/src/main/java/com/orientalSalad/troubleShot/global/encrypt/HashEncrypt.java
@@ -0,0 +1,29 @@
+package com.orientalSalad.troubleShot.global.encrypt;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HashEncrypt {
+	public String hashWithSHA256(String textToHash) {
+		try {
+			// sha-256으로 메시지 다이제스트 생성 	
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+			//해싱
+			byte[] hash = md.digest(textToHash.getBytes());
+
+			//16 진수로 변경
+			StringBuilder sb = new StringBuilder();
+			for (byte b : hash) {
+				sb.append(String.format("%02x", b));
+			}
+
+			return sb.toString();
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException("SHA-256 algorithm을 찾을 수 없습니다.", e);
+		}
+	}
+}
