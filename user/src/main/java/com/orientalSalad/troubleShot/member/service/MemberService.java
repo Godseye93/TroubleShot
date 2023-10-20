bf58daca910663168afc087511834e800330ea56
SuHyun Kim <khnemu@naver.com>
2023-10-20 17:55:48 +0900

    feat : 비밀번호를 sha-256으로 암호화
    1. 입력받은 비밀번호를 sha-256으로 암호화하여 저장하는 기능 추가
    
    231020 suhyun

--- user/src/main/java/com/orientalSalad/troubleShot/member/service/MemberService.java
+++ user/src/main/java/com/orientalSalad/troubleShot/member/service/MemberService.java
@@ -2,6 +2,7 @@ package com.orientalSalad.troubleShot.member.service;
 
 import org.springframework.stereotype.Service;
 
+import com.orientalSalad.troubleShot.global.encrypt.HashEncrypt;
 import com.orientalSalad.troubleShot.member.dto.MemberDTO;
 import com.orientalSalad.troubleShot.member.entity.MemberEntity;
 import com.orientalSalad.troubleShot.member.repository.MemberRepository;
@@ -12,8 +13,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MemberService {
 	private final MemberRepository memberRepository;
+	private final HashEncrypt hashEncrypt;
 
 	public void insertMember(MemberDTO memberDTO){
+		//sha-256으로 비밀번호 해싱
+		memberDTO.setPassword(hashEncrypt.hashWithSHA256(memberDTO.getPassword()));
+		
 		MemberEntity memberEntity = memberDTO.toMemberEntity();
 		memberRepository.save(memberEntity);
 	}
