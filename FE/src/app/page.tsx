5adb470441108e738da6a5d4c29d42adb28bb2a0
Jaehyung <thswogud02@gmail.com>
2023-10-26 17:56:57 +0900

    fix: 헤더 비율 수정 및 소개 페이지 별 기능 추가
    231926 jaehyung

--- FE/src/app/page.tsx
+++ FE/src/app/page.tsx
@@ -1,11 +1,64 @@
 import Image from "next/image";
-import browser_icon from "/public/browser_icon.png";
+import main_logo from "/public/logo/logo-no-background.png";
+import chrome_logo from "/public/logo/chrome_logo.png"
+import intelliJ_logo from "/public/logo/intelliJ_logo.png"
+import vscode_logo from "/public/logo/vscode_logo.png"
+
+import styles from '../styles/Home.module.css';
+import Link from "next/link";
+
 
 export default function Home() {
-  const id = "2";
   return (
-    <main className=" flex flex-col gap-4 items-center">
-      <Image src={browser_icon} alt="" />
-    </main>
+    <div className="bg-gradient-to-b from-black">
+
+      <main className="flex flex-col items-center gap-4 text-white">
+        {[...Array(50)].map((_, i) => (
+          <div
+            key={i}
+            className={`${styles.star} ${styles[`twinkle${i % 4 + 1}`]}`}
+            style={{
+              top: `${Math.random() * 100}%`,
+              left: `${Math.random() * 100}%`,
+              animationDelay: `${Math.random()}s`,
+            }}
+          />
+        ))}
+
+        <div className="flex flex-col items-center mb-10 w-10/12">
+          <h1 className="mt-32 mb-10 text-3xl">트러블 슈팅의 완벽한 파트너</h1>
+          <Image src={main_logo} alt="trouble-shot" className=" w-9/12" />
+          <Link href="#" className=" mt-10 mb-3 w-1/4 bg-gray-500 flex justify-center items-center text-2xl p-3">with VSC <Image src={vscode_logo} alt="vscode_logo" className=" w-10 ml-5" /></Link>
+          <Link href="#" className=" mb-3 w-1/4 bg-gray-500 flex justify-center items-center text-2xl p-3">with IntelliJ <Image src={intelliJ_logo} alt="intelliJ_logo" className=" w-10 ml-5" /></Link>
+          <Link href="#" className=" mb-3 w-1/4 bg-gray-500 flex justify-center items-center text-2xl p-3">with Chrome <Image src={chrome_logo} alt="chrome_logo" className=" w-10 ml-5" /></Link>
+        </div>
+        <div className="flex flex-col items-center">
+          <h1 className=" mt-32 text-7xl">트러블 샷의 주요 기능</h1>
+          <div className="flex">
+            <section className="flex flex-col">
+              <div className=""><button>기능 1</button></div>
+              <div className=""><button>기능 2</button></div>
+              <div className=""><button>기능 3</button></div>
+            </section>
+            <div className=" bg-orange-300 w-[1000px]" />
+          </div>
+        </div>
+        <div className="flex flex-col items-center">
+          <h1 className="mt-32 mb-10 text-3xl">트러블 슈팅의 완벽한 파트너</h1>
+          <Image src={main_logo} alt="trouble-shot" className=" w-9/12" />
+          <Link href="#" className=" mt-10 mb-3 w-72 bg-gray-500 flex justify-center items-center text-2xl p-3">with VSC <Image src={vscode_logo} alt="vscode_logo" className=" w-10 ml-5" /></Link>
+          <Link href="#" className=" mb-3 w-72 bg-gray-500 flex justify-center items-center text-2xl p-3">with IntelliJ <Image src={intelliJ_logo} alt="intelliJ_logo" className=" w-10 ml-5" /></Link>
+          <Link href="#" className=" mb-3 w-72 bg-gray-500 flex justify-center items-center text-2xl p-3">with Chrome <Image src={chrome_logo} alt="chrome_logo" className=" w-10 ml-5" /></Link>
+        </div>
+        <div className="flex flex-col items-center">
+          <h1 className="mt-32 mb-10 text-3xl">트러블 슈팅의 완벽한 파트너</h1>
+          <Image src={main_logo} alt="trouble-shot" className=" w-9/12" />
+          <Link href="#" className=" mt-10 mb-3 w-72 bg-gray-500 flex justify-center items-center text-2xl p-3">with VSC <Image src={vscode_logo} alt="vscode_logo" className=" w-10 ml-5" /></Link>
+          <Link href="#" className=" mb-3 w-72 bg-gray-500 flex justify-center items-center text-2xl p-3">with IntelliJ <Image src={intelliJ_logo} alt="intelliJ_logo" className=" w-10 ml-5" /></Link>
+          <Link href="#" className=" mb-3 w-72 bg-gray-500 flex justify-center items-center text-2xl p-3">with Chrome <Image src={chrome_logo} alt="chrome_logo" className=" w-10 ml-5" /></Link>
+        </div>
+      </main>
+    </div>
+
   );
 }
---
+++ FE/src/components/Header.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import Image from "next/image";
+import trous_logo_origin from "/public/logo/trous_logo_origin.png";
+
+
+export default function Header() {
+    return (
+        <div className='bg-white rounded-lg flex text-xl justify-between fixed top-1 left-2 right-2'>
+            <div className='flex items-center'>
+                <Link href="/" className=" w-2/12 h-2/12 mx-3"><Image src={trous_logo_origin} alt="" /></Link>
+                <Link href="/trouble/1" className='me-5 text-black'>내 트러블 슈팅</Link>
+                <Link href="/community" className='me-5 text-black'>커뮤니티</Link>
+            </div>
+            <Link href="/login" className=' bg-yellow-300 rounded-lg m-3 w-36'>Log in</Link>
+        </div>
+    );
+}
