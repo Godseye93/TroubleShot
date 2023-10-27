e4f92ffa26a1f967fc465efd0f9192bc26f99921
Jaehyung <thswogud02@gmail.com>
2023-10-27 10:57:33 +0900

    design: 메인 기능 소개 부분 컴포넌트화
    231027 jaehyung

--- FE/src/app/page.tsx
+++ FE/src/app/page.tsx
@@ -7,6 +7,7 @@ import vscode_logo from "/public/logo/vscode_logo.png"
 import styles from '../styles/Home.module.css';
 import Link from "next/link";
 
+import FeatureIntro from "../components/FeatureIntro";
 
 export default function Home() {
   return (
@@ -32,17 +33,9 @@ export default function Home() {
           <Link href="#" className=" mb-3 w-1/4 bg-gray-500 flex justify-center items-center text-2xl p-3 hover:bg-yellow-400">with IntelliJ <Image src={intelliJ_logo} alt="intelliJ_logo" className=" w-10 ml-5" /></Link>
           <Link href="#" className=" mb-3 w-1/4 bg-gray-500 flex justify-center items-center text-2xl p-3 hover:bg-yellow-400">with Chrome <Image src={chrome_logo} alt="chrome_logo" className=" w-10 ml-5" /></Link>
         </div>
-        <div className="flex flex-col w-full items-center">
-          <h1 className=" mt-32 text-7xl">트러블 샷의 주요 기능</h1>
-          <div className="flex w-full justify-center">
-            <section className="flex flex-col w-3/12">
-              <div className="bg-orange-300 hover:bg-orange-600 w-4/12 text-2xl"><button>기능 1</button></div>
-              <div className="bg-orange-300 hover:bg-orange-600 w-4/12 text-2xl"><button>기능 2</button></div>
-              <div className="bg-orange-300 hover:bg-orange-600 w-4/12 text-2xl"><button>기능 3</button></div>
-            </section>
-            <div className=" bg-orange-300" />
-          </div>
-        </div>
+
+        <FeatureIntro></FeatureIntro>
+
         <div className="flex flex-col items-center">
           <h1 className="mt-32 mb-10 text-3xl">트러블 슈팅의 완벽한 파트너</h1>
           <Image src={main_logo} alt="trouble-shot" className=" w-9/12" />
@@ -50,6 +43,7 @@ export default function Home() {
           <Link href="#" className=" mb-3 w-72 bg-gray-500 flex justify-center items-center text-2xl p-3">with IntelliJ <Image src={intelliJ_logo} alt="intelliJ_logo" className=" w-10 ml-5" /></Link>
           <Link href="#" className=" mb-3 w-72 bg-gray-500 flex justify-center items-center text-2xl p-3">with Chrome <Image src={chrome_logo} alt="chrome_logo" className=" w-10 ml-5" /></Link>
         </div>
+        
         <div className="flex flex-col items-center">
           <h1 className="mt-32 mb-10 text-3xl">트러블 슈팅의 완벽한 파트너</h1>
           <Image src={main_logo} alt="trouble-shot" className=" w-9/12" />
