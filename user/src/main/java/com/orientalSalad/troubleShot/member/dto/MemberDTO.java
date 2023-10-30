50c7ef2a3eef6842c1b760cc11196e5142f33c5f
SuHyun Kim <khnemu@naver.com>
2023-10-30 15:50:46 +0900

    fix : MemberDTO import 정리
    231030 suhyun

--- user/src/main/java/com/orientalSalad/troubleShot/member/dto/MemberDTO.java
+++ user/src/main/java/com/orientalSalad/troubleShot/member/dto/MemberDTO.java
@@ -1,14 +1,9 @@
 package com.orientalSalad.troubleShot.member.dto;
 
-import java.time.LocalDateTime;
-
 import com.orientalSalad.troubleShot.global.dto.BaseDTO;
-import com.orientalSalad.troubleShot.global.pattern.Pattern;
-import com.orientalSalad.troubleShot.member.entity.MemberEntity;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
