f2137330b0f36377cd55186162dc94df46710a70
Jaehyung <thswogud02@gmail.com>
2023-11-02 00:16:42 +0900

    fix: footer css 수정
    231102 jaehyung

--- FE/src/components/Footer.tsx
+++ FE/src/components/Footer.tsx
@@ -1,28 +1,29 @@
 "use client";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
-import blackLogo from "../../public/logo/trous_withoutLogo_black.png"
+import blackLogo from "../../public/logo/trous_withoutLogo_origin.png"
 
 export default function Footer() {
   const path = usePathname();
 
   return (
-    <div id="footer" className={`${path === "/"  && "hidden"} flex w-full border-t-2 border-gray-400`}>
-      <ul className="w-3/12">
-        <li><Image src={blackLogo} alt="" className=""/></li>
+    <div id="footer" className={`${path === "/"  && "hidden"} flex w-full border-t-2 border-gray-400 justify-between`}>
+      <ul className="w-fit mt-3 ms-3">
+        <li><Image src={blackLogo} alt="" className="w-[25vw]"/></li>
       </ul>
-      <div className="flex w-5/12">
-        <ul className="w-1/2">
-            <li><div className="">오리엔탈 샐러드</div></li>
+      <div className="flex">
+        <ul className="w-fit me-10 p-3">
+            <li className="mb-2"><p className="border-b border-main">오리엔탈 샐러드</p></li>
             <li>정슬호</li>
-            <li>정슬호</li>
-            <li>정슬호</li>
-            <li>정슬호</li>
-            <li>정슬호</li>
-            <li>정슬호</li>
+            <li>고예림</li>
+            <li>권종률</li>
+            <li>김수현</li>
+            <li>손재형</li>
+            <li>장진욱</li>
+            
         </ul>
-        <ul className="w-1/2">
-            <li>관련 사이트</li>
+        <ul className="w-fit me-5 p-3">
+            <li className="mb-2"><p className="border-b border-main">관련 사이트</p></li>
             <li>팀 노션 페이지</li>
             <li>팀 깃랩</li>
         </ul>
