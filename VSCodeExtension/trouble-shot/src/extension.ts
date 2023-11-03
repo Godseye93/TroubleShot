f7eb78bbe2f44c1807098fe68525d3fbda9a770f
Jeong seulho <wjdtmfgh@gmail.com>
2023-11-03 17:59:39 +0900

    feat : 에러 히스토리 등록

--- VSCodeExtension/trouble-shot/src/extension.ts
+++ VSCodeExtension/trouble-shot/src/extension.ts
@@ -6,6 +6,7 @@ import { getRootPath } from "./utilities/getRootPath";
 import { MyTroubleListProviderWithoutLogin } from "./TreeDataProvider/MyTroubleListProviderWithoutLogin";
 import { getMarkdownView } from "./panels/getMarkdownView";
 import { Trouble } from "./TreeDataProvider/MyTroubleListProvider";
+import { spawn } from "child_process";
 
 export const TROUBLE_SHOOTING_TYPE = {
   TROUBLE: 0 as const,
@@ -14,9 +15,40 @@ export const TROUBLE_SHOOTING_TYPE = {
 };
 
 export async function activate(context: vscode.ExtensionContext) {
+  const rootPath = getRootPath();
   const sessionId = await getSessionId(context);
   const isLogin = sessionId !== -1;
   vscode.commands.executeCommand("setContext", "isLogin", isLogin);
+
+  context.subscriptions.push(
+    vscode.workspace.onDidSaveTextDocument((document) => {
+      const buildProcess = spawn("npm", ["run", "build", "--", "--logLevel", "error"], {
+        cwd: rootPath,
+        shell: true,
+      });
+
+      buildProcess.on("error", (err) => {
+        console.error("Failed to start subprocess.", err);
+      });
+
+      let buildErrors = "";
+      let allOutput = "";
+
+      buildProcess.stderr.on("data", (data) => {
+        buildErrors += data.toString();
+      });
+
+      buildProcess.stdout.on("data", (data) => {
+        allOutput += data.toString();
+      });
+
+      buildProcess.on("close", (code) => {
+        if (code !== 0) {
+          console.error(`Build failed with code ${code} and errors:\n${buildErrors}`);
+        }
+      });
+    })
+  );
 
   // trouble webview panel
   context.subscriptions.push(
@@ -66,7 +98,6 @@ export async function activate(context: vscode.ExtensionContext) {
   );
 
   // dependencies tree view
-  const rootPath = getRootPath();
   const nodeDependenciesProvider = new NodeDependenciesProvider(rootPath);
   context.subscriptions.push(
     vscode.window.registerTreeDataProvider("node-dependencies", nodeDependenciesProvider)
