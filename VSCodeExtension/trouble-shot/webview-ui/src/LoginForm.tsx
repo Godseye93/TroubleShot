da0509c53d199a0db3c5bd6510ca039e9027fd26
Jeong seulho <wjdtmfgh@gmail.com>
2023-11-03 11:11:24 +0900

    feat : login form

--- VSCodeExtension/trouble-shot/webview-ui/src/LoginForm.tsx
+++ VSCodeExtension/trouble-shot/webview-ui/src/LoginForm.tsx
@@ -1,15 +1,36 @@
 import { VSCodeButton, VSCodeTextField } from "@vscode/webview-ui-toolkit/react";
 import { BiLogIn } from "react-icons/bi";
+import { useState } from "react";
 
 const LoginForm = () => {
+  const [loginForm, setLoginForm] = useState({
+    id: "",
+    password: "",
+  });
+  function onChange(e: any) {
+    setLoginForm((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  }
+  function onLogin() {
+    console.log(loginForm);
+  }
   return (
     <section className="flex flex-col w-2/3 gap-3 ">
-      <VSCodeTextField>ID</VSCodeTextField>
-      <VSCodeTextField>PASS WORD</VSCodeTextField>
-      <VSCodeButton>
-        <BiLogIn className="mr-3 " />
-        LOGIN
-      </VSCodeButton>
+      <VSCodeTextField value={loginForm.id} name="id" onInput={onChange}>
+        ID
+      </VSCodeTextField>
+      <VSCodeTextField
+        value={loginForm.password}
+        name="password"
+        onInput={onChange}
+        type="password">
+        PASS WORD
+      </VSCodeTextField>
+      <div onClick={onLogin}>
+        <VSCodeButton>
+          <BiLogIn className="mr-3 " />
+          LOGIN
+        </VSCodeButton>
+      </div>
     </section>
   );
 };
