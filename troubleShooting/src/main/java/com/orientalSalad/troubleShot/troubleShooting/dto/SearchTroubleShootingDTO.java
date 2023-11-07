fb94aafe79d0ab4fecfbbd5d062f95908e94bc9a
SuHyun Kim <khnemu@naver.com>
2023-11-07 09:35:34 +0900

    fix : 트러블 슈팅 문서 개수 오류 해결
    231107 suhyun

--- troubleShooting/src/main/java/com/orientalSalad/troubleShot/troubleShooting/dto/SearchTroubleShootingDTO.java
+++ troubleShooting/src/main/java/com/orientalSalad/troubleShot/troubleShooting/dto/SearchTroubleShootingDTO.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 
