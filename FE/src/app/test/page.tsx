27a9e0ed3196131ad6062a03dc255ba940b1698f
Jonryul <kjr5876@gmail.com>
2023-11-16 23:40:42 +0900

    design : 스켈레톤 ui 적용중
    20231116 jongryul

--- FE/src/app/test/page.tsx
+++ FE/src/app/test/page.tsx
@@ -1,10 +1,17 @@
+import ProfileSkeleton from "@/components/Skeletons/ProfileSkeleton";
+import RsidebarSkeleton from "@/components/Skeletons/RsidebarSkeleton";
 import SidebarSkeleton from "@/components/Skeletons/SidebarSkeleton";
 export default function Page() {
   return (
     <main className="mx-3 flex-1">
       <div className="h-12"></div>
-
-      <SidebarSkeleton />
+      <div className="flex justify-between">
+        <SidebarSkeleton />
+        <div>
+          <ProfileSkeleton />
+          <RsidebarSkeleton />
+        </div>
+      </div>
     </main>
   );
 }
