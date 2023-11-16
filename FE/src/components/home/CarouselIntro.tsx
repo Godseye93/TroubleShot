e90928bf92d54f8f8e9ac3a6148d47b4b935f233
Jaehyung <thswogud02@gmail.com>
2023-11-16 10:10:40 +0900

    fix: 캐러셀 css 수정
    231116 jaehyung

--- FE/src/components/home/CarouselIntro.tsx
+++ FE/src/components/home/CarouselIntro.tsx
@@ -2,27 +2,27 @@
 import React, { MouseEvent, UIEvent, useState } from "react";
 import Image from "next/image";
 
-import exImage1 from "../../../public/carousel/seul.jpg";
+import seul from "../../../public/carousel/seul.jpg";
 import exImage2 from "../../../public/carousel/exImage2.jpg";
 import exImage3 from "../../../public/carousel/exImage3.jpg";
 import exImage4 from "../../../public/carousel/exImage4.jpg";
 import exImage5 from "../../../public/carousel/exImage5.jpg";
-import { IoArrowBackCircleSharp } from "react-icons/io5";
-import { IoArrowForwardCircleSharp } from "react-icons/io5";
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
 
 export default function CarouselIntro() {
   const imageList = [
-    { index: 0, Image: exImage1, text: "팀장 정슬호" },
-    { index: 1, Image: exImage2, text: "정슬호입니다." },
-    { index: 2, Image: exImage3, text: "테스트테스트" },
-    { index: 3, Image: exImage4, text: "원투원투쓰리" },
-    { index: 4, Image: exImage5, text: "다섯번째캐러셀" },
+    { index: 0, Image: seul, name: "슬호", text: "정슬호 팀장", review: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa" },
+    { index: 1, Image: exImage2, name: "예림", text: "고예림 팀원", review: "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb" },
+    { index: 2, Image: exImage3, name: "수현", text: "김수현 팀원", review: "ccccccccccccccccccccccccccccccccccc" },
+    { index: 3, Image: exImage4, name: "종률", text: "권종률 팀원", review: "dddddddddddddddddddddddddddddddddddd" },
+    { index: 4, Image: exImage5, name: "재형", text: "손재형 팀원", review: "eeeeeeeeeeeeeeeeeeeeeeeeeeeeee" },
+    { index: 5, Image: exImage5, name: "진욱", text: "장진욱 팀원", review: "fffffffffffffffffffffffffffffffffffffff" },
   ];
   const [current, setCurrent] = useState(0);
 
   const nextHandler = () => {
     setCurrent(() => {
-      if (current > 3) {
+      if (current > 4) {
         return 0;
       } else {
         return current + 1;
@@ -42,10 +42,11 @@ export default function CarouselIntro() {
 
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const [visible, setVisible] = useState(false);
-  const [cursorText, setCursorText] = useState("팀 오리엔탈 샐러드");
+  const [cursorText, setCursorText] = useState("오리엔탈 샐러드");
   const [customCursor, setCustomCursor] = useState("custom-cursor");
 
-  const trackCursor = (event) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const trackCursor = (event: any) => {
     setPosition({ x: event.clientX, y: event.clientY });
     setVisible(true);
   };
@@ -63,11 +64,11 @@ export default function CarouselIntro() {
   };
 
   return (
-    <div className="w-full h-3/4">
-      <h1 className="w-full text-3xl font-bold m-5 ms-10">만든 사람들</h1>
+    <div className="w-full h-3/4 flex flex-col justify-center">
+      <h1 className="w-full text-3xl font-bold  m-5 ms-10">만든 사람들</h1>
       <div
         id="carouselIntor"
-        className="w-full h-3/4 bg-softmain flex "
+        className="w-full h-3/4 border border-main rounded flex "
         onMouseMove={trackCursor}
         onMouseLeave={hideCursor}
       >
@@ -80,54 +81,62 @@ export default function CarouselIntro() {
           </div>
         )}
         <div
-          className="w-3/12 active:bg-orange-400 "
+          className="w-3/12 active:bg-gray-200"
           onClick={prevHandler}
           onMouseEnter={() => {
             changeCursorText("이전으로");
             changeCss("custom-cursor-left");
           }}
           onMouseLeave={() => {
-            changeCursorText("팀 오리엔탈 샐러드");
+            changeCursorText("오리엔탈 샐러드");
             changeCss("custom-cursor");
           }}
         ></div>
-        <div className="w-6/12 flex justify-between items-center">
+        <div className="w-6/12 flex justify-between items-center opacity-100">
           {imageList.map((item, index) => (
-            <div className="w-2/6" key={index} style={{ display: index === current ? "block" : "none" }}>
+            <div className="w-2/6 opacity-100" key={index} style={{ display: index === current ? "block" : "none" }}>
               <Image className="rounded-lg" src={item.Image} alt={item.text} />
+              <div className="w-full mt-3 text-2xl text-center font-bold">{item.text}</div>
             </div>
           ))}
-          {imageList.map((item, index) => (
-            <div className=" w-3/6 me-5 " key={index} style={{ display: index === current ? "block" : "none" }}>
-              <div className="w-fit text-4xl ">{item.text}</div>
-            </div>
-          ))}
+          <div className="opacity-100">
+            <FaQuoteLeft />
+            {imageList.map((item, index) => (
+              <div
+                className=" w-3/6 me-5 h-full flex flex-col justify-between"
+                key={index}
+                style={{ display: index === current ? "block" : "none" }}
+              >
+                {item.review}
+              </div>
+            ))}
+            <FaQuoteRight />
+          </div>
         </div>
         <div
-          className="w-3/12 active:bg-orange-400"
+          className="w-3/12 active:bg-gray-200"
           onClick={nextHandler}
           onMouseEnter={() => {
             changeCursorText("앞으로");
             changeCss("custom-cursor-right");
           }}
           onMouseLeave={() => {
-            changeCursorText("팀 오리엔탈 샐러드");
+            changeCursorText("오리엔탈 샐러드");
             changeCss("custom-cursor");
           }}
         ></div>
       </div>
+      <ul className="flex w-full justify-center gap-4 mb-2 list-none">
+        {imageList.map((v, idx) => (
+          <li
+            key={idx}
+            className={`text-lg ${idx === current ? "opacity-100 font-bold" : "opacity-50"}`}
+            onClick={() => setCurrent(idx)}
+          >
+            {v.name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
-
-{
-  /* <ul className="flex w-full justify-center gap-4 mb-2">
-          {imageList.map((_, idx) => (
-            <li
-              key={idx}
-              className={`h-[1.2rem] w-[1.2rem] rounded-full bg-sub ${idx === current ? "opacity-100" : "opacity-50"}`}
-              onClick={() => setCurrent(idx)}
-            />
-          ))}
-        </ul> */
 }
