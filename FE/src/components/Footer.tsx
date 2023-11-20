e7d0015099ccaf3c2a7fee736bc80d1084df5ea6
Jeong seulho <wjdtmfgh@gmail.com>
2023-11-20 17:48:26 +0900

    design : footer 축소

--- FE/src/components/Footer.tsx
+++ FE/src/components/Footer.tsx
@@ -17,41 +17,43 @@ export default function Footer() {
           <Image src={blackLogo} alt="" className="w-[25vw]" />
         </li>
       </ul>
-      <div className="flex">
+      <div className="flex mr-56">
         <ul className="w-fit me-10 p-3 list-none">
           <li className="mb-2">
             <p className="border-b border-main">오리엔탈 샐러드</p>
           </li>
-          <li>
-            <Link href={"https://github.com/Jeongseulho"} target="_blank">
-              정슬호
-            </Link>
-          </li>
-          <li>
-            <Link href={"https://github.com/YeahLim"} target="_blank">
-              고예림
-            </Link>
-          </li>
-          <li>
-            <Link href={"https://github.com/KwonJongryul"} target="_blank">
-              권종률
-            </Link>
-          </li>
-          <li>
-            <Link href={"https://github.com/khnemu11"} target="_blank">
-              김수현
-            </Link>
-          </li>
-          <li>
-            <Link href={"https://github.com/sonesonjabgo"} target="_blank">
-              손재형
-            </Link>
-          </li>
-          <li>
-            <Link href={"https://github.com/Godseye93"} target="_blank">
-              장진욱
-            </Link>
-          </li>
+          <div className=" flex gap-4">
+            <li>
+              <Link href={"https://github.com/Jeongseulho"} target="_blank">
+                정슬호
+              </Link>
+            </li>
+            <li>
+              <Link href={"https://github.com/YeahLim"} target="_blank">
+                고예림
+              </Link>
+            </li>
+            <li>
+              <Link href={"https://github.com/KwonJongryul"} target="_blank">
+                권종률
+              </Link>
+            </li>
+            <li>
+              <Link href={"https://github.com/khnemu11"} target="_blank">
+                김수현
+              </Link>
+            </li>
+            <li>
+              <Link href={"https://github.com/sonesonjabgo"} target="_blank">
+                손재형
+              </Link>
+            </li>
+            <li>
+              <Link href={"https://github.com/Godseye93"} target="_blank">
+                장진욱
+              </Link>
+            </li>
+          </div>
         </ul>
         <ul className="w-fit me-5 p-3 list-none">
           <li className="mb-2">
