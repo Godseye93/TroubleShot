0b079e7aed3b7e498acdd7dd3d7b2ebc2e283716
Jonryul <kjr5876@gmail.com>
2023-11-22 09:56:24 +0900

    fix : 검색결과 없을 시 푸터 올라가는 버그, 검색결과 새로고침 시 작성자가 제목검색으로 바뀌는 버그 수정
    20231122 jongryul

--- FE/src/components/Sidebar.tsx
+++ FE/src/components/Sidebar.tsx
@@ -24,8 +24,6 @@ export default function Sidebar({ menus, categories, link, isLogged, userSeq }: 
       const val = document.documentElement.scrollHeight - window.innerHeight - footer.offsetHeight;
 
       if (sT > 0 && sT >= val) {
-        console.log("st", sT);
-        console.log("val", val);
         sidebar.classList.add("on");
       } else {
         sidebar.classList.remove("on");
@@ -38,7 +36,7 @@ export default function Sidebar({ menus, categories, link, isLogged, userSeq }: 
     };
   }, []);
   return (
-    <div className="w-[14%] h-full md:flex hidden">
+    <div className="w-[14%] min-h-[91vh] md:flex hidden">
       <div
         className=" w-[14%] h-[91vh] bg-white shadow-lg mt-4 rounded-lg pt-5  flex-col justify-between text-lg md:flex hidden"
         id="sidebar"
